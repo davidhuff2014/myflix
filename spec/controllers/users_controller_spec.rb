0a97b21c5a824ae112f2745b75e41e66aa6ba090
davidhuff2014 <David.Huff@computer-critters.com>
2014-08-06 15:54:56 -0400

    after controller tests for authentication

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,26 +2,45 @@ require 'spec_helper'
 
 describe UsersController do
   describe 'GET new' do
-    it 'sets the @user variable' do
+    it 'sets @user' do
       get :new
-      assigns(:user).should be_new_record
-      assigns(:user).should be_instance_of(User)
-    end
-    it 'renders the new template' do
-      get :new
-      response.should  render_template :new
+      expect(assigns(:user)).to be_instance_of(User)
     end
   end
 
   describe 'POST create' do
-    let(:user) { Fabricate(:user) }
-    it { user.should validate_presence_of(:email) }
-    it { user.should validate_presence_of(:password_digest) }
-    it { user.should validate_presence_of(:full_name) }
+    context 'with valid input' do
 
-    it { user.should validate_uniqueness_of(:email) }
+      before do
+        post :create, user: Fabricate.attributes_for(:user)
+      end
 
-    it { should redirect_to_url }
-    it 'renders the new template when the input is invalid'
+      it 'creates the user' do
+        expect(User.count).to eq(1)
+      end
+
+      it 'redirects to the sign in page' do
+        expect(response).to redirect_to sign_in_path
+      end
+    end
+
+    context 'with invalid input' do
+
+      before do
+        post :create, user: { password: 'password', full_name: 'Jane Doe' }
+      end
+
+      it 'does not create the user' do
+        expect(User.count).to eq(0)
+      end
+
+      it 'renders the :new template' do
+        expect(response).to render_template :new
+      end
+
+      it 'sets @user' do
+        expect(assigns(:user)).to be_instance_of(User)
+      end
+    end
   end
 end
