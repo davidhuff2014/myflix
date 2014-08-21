2b4a8ce405fc17403992051897872da202f64561
davidhuff2014 <David.Huff@computer-critters.com>
2014-08-21 17:39:26 -0400

    People I follow page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Myflix::Application.routes.draw do
 
   resources :users, only: [:show]
   get 'people', to: 'relationships#index'
+  resources :relationships, only: [:destroy]
 
   resources :categories, only: [:show]
 
--- spec/controllers/relationship_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe RelationshipController do
-
-  describe 'GET index' do
-    it 'sets @relationships to the current_user following relationships'
-  end
-end
-# TODO hw4-2 5:04
