07579dafefaa36ac0830ca0e7995ed22fe0fb030
davidhuff2014 <David.Huff@computer-critters.com>
2014-08-27 18:27:30 -0400

    add code to handle iLike for pg

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,6 +5,17 @@ class Video < ActiveRecord::Base
 
   def self.search_by_title(search_term)
     return [] if search_term.blank?
-    where('title LIKE ?', "%#{search_term}%").order('created_at DESC')
+    adapter_type = connection.adapter_name.downcase.to_sym
+    case adapter_type
+      when :mysql
+        # do the MySQL part
+      when :sqlite
+        where('title LIKE ?', "%#{search_term}%").order('created_at DESC')
+      when :postgresql
+        where('title iLIKE ?', "%#{search_term}%").order('created_at DESC')
+      else
+        raise NotImplementedError, "Unknown adapter type '#{adapter_type}'"
+    end
+
   end
 end
