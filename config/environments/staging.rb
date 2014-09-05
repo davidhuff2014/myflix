8d8aa5fc2cec86441c00f34b88ffe6aed9cdf77c
davidhuff2014 <David.Huff@computer-critters.com>
2014-09-05 14:55:13 -0400

    change staging to stage default_url_options

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -18,7 +18,7 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { host: 'http://mysterious-lowlands-6704-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://mysterious-lowlands-6704-stage.herokuapp.com' }
 
   config.action_mailer.delivery_method = :letter_opener
 
