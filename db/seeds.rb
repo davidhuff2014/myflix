1f70a44fe453ae9ca3a937fd4e2dc31b9d887242
davidhuff2014 <David.Huff@computer-critters.com>
2014-09-19 13:48:09 -0400

    updates after review

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,10 @@ User.create(full_name: 'Dave Huff', password: 'password', email: 'David.Huff@com
 
 categories = Category.create([{ name: 'TV Commedies' }, { name: 'TV Dramas' }, { name: 'Reality TV' }])
 
-# TODO need to build up seed data for videos
-Video.create(title: 'Breaking Bad - Pilot',
-             description: 'A mild mannered teacher of high school chemistry learns he' +
-                 'has contracted lung cancer and decides to undertake a new ' +
-                 'career in the manufacture of crystal meth as a way of ' +
-                 'guaranteeing financial solvency for his wife and son' +
-                 'after his death.',
-             small_cover: File.open(File.join(Rails.root, '/spec/support/uploads/breaking_bad_small.jpg')),
-             large_cover: File.open(File.join(Rails.root, '/spec/support/uploads/breaking_bad_large.jpg')),
-             category: drama,
-             video_url: 'https://s3-us-west-1.amazonaws.com/brandons-myflix-dev/uploads/video/static/Authentic+Longfield+Super+Axles.mp4'
-)
+Video.create(title: 'Monk', description: 'Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way.', small_cover: File.open(File.join(Rails.root, '/spec/support/uploads/monk.jpg')), large_cover: File.open(File.join(Rails.root, '/spec/support/uploads/monk_large.jpg')), category_id: 2, video_url: 'https://s3-us-west-2.amazonaws.com/davidhuff2014-myflix/MONK+-+Extended+Intro-Rwz24jB96fY.mp4')
 
+Video.create(title: 'Family Guy', description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.', small_cover: File.open(File.join(Rails.root, '/spec/support/uploads/family_guy.jpg')), large_cover: File.open(File.join(Rails.root, '/spec/support/uploads/family_guy_large.jpg')), category_id: 1, video_url: 'https://s3-us-west-2.amazonaws.com/davidhuff2014-myflix/Family+Guy+-+Simpsons+Crossover+-+Episode+-+%27OFFICIAL+TRAILER%27-TnhcbZmmB0U.mp4')
+
+Video.create(title: 'Futurama', description: "Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999.", small_cover: File.open(File.join(Rails.root, '/spec/support/uploads/futurama.jpg')), large_cover: File.open(File.join(Rails.root, '/spec/support/uploads/futurama_large.jpg')), category_id: 1, video_url: 'https://s3-us-west-2.amazonaws.com/davidhuff2014-myflix/Futurama+-+Bender%27s+Big+Score+Trailer-oEiJ3QA4kKs.mp4')
+
+Video.create(title: 'South Park', description: 'Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.', small_cover: File.open(File.join(Rails.root,  '/spec/support/uploads/south_park.jpg')), large_cover: File.open(File.join(Rails.root, '/spec/support/uploads/south_park_large.jpg')), category_id: 1, video_url: 'https://s3-us-west-2.amazonaws.com/davidhuff2014-myflix/South+Park+-+The+Stick+of+Truth+E3+Official+Trailer-bkZHv-9e2ro.mp4')
