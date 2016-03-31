7545b4247de4983a982b24cc31d745354fb4b77a
David Huff <David.Huff@computer-critters.com>
2016-03-31 19:03:56 -0400

    Get rid of case sensitive email problem.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password) }
   it { should validate_presence_of(:full_name) }
-  it { should validate_uniqueness_of(:email), { case_sensitive: false } }
+  it { should validate_uniqueness_of(:email), { :case_sensitive => false } }
   it { should have_many(:queue_items), -> { order 'position' } }
   it { should have_many(:reviews), -> { order 'created_at DESC' } }
 
