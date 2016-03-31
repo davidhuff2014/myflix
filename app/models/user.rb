e46c59f9cff739cd77f80965f8a6fee81dc83541
David Huff <David.Huff@computer-critters.com>
2016-03-31 18:53:49 -0400

    Get rid of case sensitive email problem.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   include Tokenable
 
   validates_presence_of :email, :password, :full_name
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, case_sensitive: false
   has_secure_password validations: false
   has_many :queue_items, -> { order 'position' }
   has_many :reviews, -> { order 'created_at DESC' }
